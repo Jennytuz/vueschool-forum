3e1d62755ec375de89c3fb4dc68b49f887fbf616
Jennytuz <490052745@qq.com>
2021-11-25 21:56:35 +0800

    connect to firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+module.exports = {
+  apiKey: 'AIzaSyCuQTWlMx43prsZKxgJvCQPW4zHxjzNkBc',
+  authDomain: 'vue-school-forum-86e94.firebaseapp.com',
+  projectId: 'vue-school-forum-86e94',
+  storageBucket: 'vue-school-forum-86e94.appspot.com',
+  messagingSenderId: '1024146609962',
+  appId: '1:1024146609962:web:8bca32f993d24476813053'
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import firebase from 'firebase'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from '@/config/firebase'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 const forumApp = createApp(App)
 
